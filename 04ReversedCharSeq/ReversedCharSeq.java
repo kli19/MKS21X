cdc9fbcecf82dcc02c930c1bf927df1c8e0c4cf3
Karen Li <kli19@stuy.edu>
2017-11-26 18:23:30 -0500

    wrote body and test cases for charAt, length, subSequence, and toString

--- 04ReversedCharSeq/ReversedCharSeq.java
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -18,8 +18,23 @@ public class ReversedCharSeq{
 	return str.length();
     }
 
-    public String subSequence(){
-	return "";
+    public String subSequence(int start, int end){
+	return str.substring(start, end);
+    }
+
+    public String toString(){
+	return str;
+    }
+
+
+    public static void main(String[]args){
+	
+	ReversedCharSeq s = new ReversedCharSeq("hello world");
+	System.out.println(s.length() + ".... expecting 11");
+	System.out.println(s + ".... expecting dlrow olleh");
+	System.out.println(s.charAt(1) + ".... expecting l");
+	System.out.println(s.subSequence(2, 7) + ".... expecting row o");
+
     }
 
 }
