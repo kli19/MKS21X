0237cd8f9861f4bc2b2395a7512db0ee1ddeb3b5
kli19 <kli19@stuy.edu>
2017-11-28 15:32:54 -0500

    started LibraryBook

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,4 +1,12 @@
 public abstract class LibraryBook extends Book implements Comparable <LibraryBook>{
     private int callNumber;
 
+    public LibraryBook(String author, String title, String ISBN, String callNumber){
+	super(author, title, ISBN);
+	this.callNumber = callNumber;
+    }
+
+    public String getCallNumber(){
+	return callNumber;
+    }
 }
