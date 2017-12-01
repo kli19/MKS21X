381cff4aa01088f9e270f0b4a2f93ea98ca5a37a
Karen Li <kli19@stuy.edu>
2017-12-01 01:07:46 -0500

    Finished writing CirculatingBook and tested with Library

--- 05Library/CirculatingBook.java
+++ 05Library/CirculatingBook.java
@@ -15,16 +15,28 @@ public class CirculatingBook extends LibraryBook{
     public void setDueDate(String dueDate){this.dueDate = dueDate;}
 
     public void checkout(String patron, String due){
-
+	setCurrentHolder(patron);
+	setDueDate(due);
     }
 
     public void returned(){
-
+	setCurrentHolder(null);
+	setDueDate(null);
     }
 
     public String circulationStatus(){
-	//placeholder so code will compile
-	return "book available on shelves";
+	//not sure
+	if (getCurrentHolder()==null){
+	    return "book available on shelves";
+	}
+
+	return getCurrentHolder() + ", " + getDueDate();
+    }
+
+    public String toString(){
+	return
+	    super.toString() + ", " +
+	    circulationStatus();
     }
 
 
