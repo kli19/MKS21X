1a56417b7c24c8752b934547b6a29be442a3018e
Karen Li <kli19@stuy.edu>
2017-12-02 23:59:14 -0500

    implemented all methods and tested using main

--- 07BarCode/BarCode.java
+++ 07BarCode/BarCode.java
@@ -1,9 +1,15 @@
 public class Barcode implements Comparable<Barcode>{
     private String[] dictionary = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
     private String zip;
+    private String code;
 
     public Barcode(String zip){
 	this.zip = zip;
+	String temp = "";
+	for (int i = 0; i < zip.length(); i++){
+	    temp += dictionary[Integer.parseInt(zip.substring(i, i+1))];
+	}
+	code = temp;
     }
 
     public String getZip(){
@@ -11,11 +17,11 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public String getCode(){
-	return "";
+	return code;
     }
 
     public String toString(){
-	return getZip() + "\n" + getCode();
+	return getZip() + checksum() + "\n" + getCode();
     }
 
 	
@@ -28,6 +34,25 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public int checksum(){
-	return 0;
+	int sum = 0;
+	for (int i = 0; i < zip.length(); i++){
+	    sum += Integer.parseInt(zip.substring(i, i+1));
+	}
+	return sum % 10 ;
+    }
+
+    public static void main(String[]args){
+    //	Barcode a = new Barcode("11592");
+    //	Barcode b = new Barcode("11592");
+    //	Barcode c = new Barcode("00000");
+    //
+    //	System.out.println("a: " + a);
+    //	System.out.println("b: " + b);
+    //	System.out.println("c: " + c);
+    //
+    //	System.out.println(a.equals(b)); //true
+    //	System.out.println(a.equals(c)); //false
+	
+	
     }
 }
