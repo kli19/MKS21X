2660c00e5d4c7f490fc2948b6f8a1fbb89d3afb9
Karen Li <kli19@stuy.edu>
2017-12-02 20:59:43 -0500

    created separate file superArrayIterator and placeholder methods

---
+++ 06SuperArrayIterator/superArrayIterator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class superArrayIterator implements Iterator<String> {
+    public String next(){
+	return "";
+    }
+
+    public boolean hasNext(){
+	return true;
+    }
+}
