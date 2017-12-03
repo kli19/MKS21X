f4216478f772a1f3cc5c71d9e8129ba88ae40972
Karen Li <kli19@stuy.edu>
2017-12-02 23:23:19 -0500

    skeleton functions + still need to write checksum and getCode

--- 07BarCode/BarCode.java
+++ 07BarCode/BarCode.java
@@ -1,7 +1,32 @@
 public class Barcode implements Comparable<Barcode>{
+    private String zip;
 
+    public Barcode(String zip){
+	this.zip = zip;
+    }
 
+    public String getZip(){
+	return zip;
+    }
+
+    public String getCode(){
+	return "";
+    }
+
+    public String toString(){
+	return getZip() + "\n" + getCode();
+    }
+
+	
     public int compareTo(Barcode other){
+	return this.getZip().compareTo(other.getZip());
+    }
+
+    public Boolean equals(Barcode other){
+	return compareTo(other) == 0;
+    }
+
+    public int checksum(){
 	return 0;
     }
 }
