619a9d4b7176105d767bffc66e3bfa71643ea92c
Karen Li <kli19@stuy.edu>
2017-12-05 23:30:58 -0500

    wrote and tested selectionSort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,14 +1,38 @@
+import java.util.Arrays;
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
-  */
-  public static String name(){
-    return "01.Li.Karen"; 
-  }
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "01.Li.Karen"; 
+    }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){  }
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){
+      
+	for (int i = 0; i < data.length; i++){
+	    int next = data[i];
+	    int index = i;
+	    for (int j = i; j < data.length; j++){
+		if (data[j] < next){
+		    next = data[j];
+		    index = j;
+		}	      
+	    }
+	    data[index] = data[i];
+	    data[i] = next;	  
+	}
+    }
 
+
+    public static void main(String[]args){
+	int[] a = {64, 25, 12, 22, 11};
+	System.out.println(Arrays.toString(a));
+
+	selectionSort(a);
+	System.out.println(Arrays.toString(a));
+	
+    }
 }
