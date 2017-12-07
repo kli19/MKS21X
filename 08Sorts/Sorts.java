2abe557a3c334655150b3ce42063657a7a406e23
Karen Li <kli19@stuy.edu>
2017-12-06 21:24:31 -0500

    wrote and tested insertionSort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 public class Sorts{
-    /**EDIT THIS METHOD TO MATCH YOUR NAME 
-     */
+
     public static String name(){
 	return "10.Li.Karen"; 
     }
@@ -14,7 +13,6 @@ public class Sorts{
 	}
 	return true;
     }
-
     
     public static void bogoSort(int[]ary){
 	while(!isSorted(ary)){
@@ -27,10 +25,6 @@ public class Sorts{
 	}
     }
 
-    /**Selection sort of an int array. 
-     *Upon completion, the elements of the array will be in increasing order.
-     *@param data  the elements to be sorted.
-     */
     public static void selectionSort(int[] data){
       
 	for (int i = 0; i < data.length; i++){
@@ -47,10 +41,23 @@ public class Sorts{
 	}
     }
 
-
+    public static void insertionSort(int[] data){
+	for (int i = 1; i < data.length; i++){
+	    int temp = data[i]; 
+	    int index = i; 
+	    for (int j = i-1; j >= 0 && data[j]>temp; j--){
+		data[j+1] = data[j]; 
+		index = j;
+	    }
+	    data[index] = temp;
+	}
+    }
     public static void main(String[]args){
 	// testing selectionSort
-	//int[] a = {64, 25, 12, 22, 11};
+	//int[] a = new int[5];
+	//for (int i = 0; i < a.length; i++){
+	//    a[i] = (int)(Math.random()*100);
+	//}
 	//System.out.println(Arrays.toString(a)); //[64, 25, 12, 22, 11]
 	//System.out.println(isSorted(a)); //false
 	//selectionSort(a); 
@@ -62,10 +69,21 @@ public class Sorts{
 	//for (int i = 0; i < randish.length; i++){
 	//    randish[i] = (int)(Math.random()*100);
 	//}
-
 	//System.out.println(Arrays.toString(randish));
 	//bogoSort(randish);
 	//System.out.println(Arrays.toString(randish));
+
+	// testing insertionSort
+	int[] ary = new int[5];
+	for (int i = 0; i < ary.length; i++){
+	    ary[i] = (int)(Math.random()*100);
+	}
+	System.out.println(Arrays.toString(ary));
+	insertionSort(ary);
+	System.out.println(Arrays.toString(ary));
+
+	
+
     }
   
 }
