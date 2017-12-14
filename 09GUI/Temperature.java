cf017447382a6216519e6bdfaab2590f146cd287
Karen Li <kli19@stuy.edu>
2017-12-14 00:52:21 -0500

    window pops up

--- 09GUI/Temperature.java
+++ 09GUI/Temperature.java
@@ -4,10 +4,25 @@ import java.awt.event.*;
 
 public class Temperature extends JFrame{
 
+    private Container pane;
+    private JTextField text;
+    private JButton FToC;
+    private JButton CToF;
+    
     public Temperature(){
+	this.setTitle("Temperature");
+	this.setSize(1000,500);
+	this.setLocation(200,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
     }
 
-    
+
+    public static void main(String[]args){
+	Temperature x = new Temperature();
+	x.setVisible(true);
+    }
 }
 
